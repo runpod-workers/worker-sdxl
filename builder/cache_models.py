1613ba79ee75a3bccc8eb4b097689b222d182253
Justin Merrell <merrelljustin@gmail.com>
2023-08-08 11:57:33 -0400

    Update cache_models.py

--- builder/cache_models.py
+++ builder/cache_models.py
@@ -4,16 +4,36 @@ import torch
 from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
 
 
-def get_diffusion_pipelines():
-    pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",
-                                                     torch_dtype=torch.float16,
-                                                     variant="fp16",
-                                                     use_safetensors=True)
+def fetch_pretrained_model(model_class, model_name, **kwargs):
+    '''
+    Fetches a pretrained model from the HuggingFace model hub.
+    '''
+    max_retries = 3
+    for attempt in range(max_retries):
+        try:
+            return model_class.from_pretrained(model_name, **kwargs)
+        except OSError as err:
+            if attempt < max_retries - 1:
+                print(
+                    f"Error encountered: {err}. Retrying attempt {attempt + 1} of {max_retries}...")
+            else:
+                raise
 
-    refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-1.0",
-                                                               torch_dtype=torch.float16,
-                                                               variant="fp16",
-                                                               use_safetensors=True)
+
+def get_diffusion_pipelines():
+    '''
+    Fetches the Stable Diffusion XL pipelines from the HuggingFace model hub.
+    '''
+    common_args = {
+        "torch_dtype": torch.float16,
+        "variant": "fp16",
+        "use_safetensors": True
+    }
+
+    pipe = fetch_pretrained_model(StableDiffusionXLPipeline,
+                                  "stabilityai/stable-diffusion-xl-base-1.0", **common_args)
+    refiner = fetch_pretrained_model(StableDiffusionXLImg2ImgPipeline,
+                                     "stabilityai/stable-diffusion-xl-refiner-1.0", **common_args)
 
     return pipe, refiner
 
