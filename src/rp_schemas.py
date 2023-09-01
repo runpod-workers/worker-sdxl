00d04b58b215416d9007276bcc130ae1b1ee9bc1
Justin Merrell <merrelljustin@gmail.com>
2023-09-01 12:06:34 -0400

    Update rp_schemas.py

--- src/rp_schemas.py
+++ src/rp_schemas.py
@@ -56,6 +56,7 @@ INPUT_SCHEMA = {
     'num_images': {
         'type': int,
         'required': False,
-        'default': 1
+        'default': 1,
+        'constraints': lambda img_count: 3 > img_count > 0
     }
 }
